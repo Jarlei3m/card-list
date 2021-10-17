c87fb41ef827f1aafd3241df1964a8e8c546669b
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2021-10-17 09:18:40 -0300

    card list page

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+const paginate = (followers) => {
+    const followersPerPage = 9;
+    const pages = Math.ceil(followers.length /followersPerPage);
+
+    const newFollowers = Array.from({length:pages}, (_, index) => {
+        const start = index *followersPerPage;
+        
+        return followers.slice(start, start + followersPerPage)
+    });
+
+    return newFollowers;
+}
+
+export default paginate;
